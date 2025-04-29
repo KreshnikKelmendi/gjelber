b3fa1a51a097ecfc6e0c0f0ffcf56ce01240ffab
KreshnikKelmendi <kreshnik.kelmendi1994@gmail.com>
2025-04-29 13:23:22 +0200

    d

--- src/components/data/shpalljet.js
+++ src/components/data/shpalljet.js
@@ -70,8 +70,41 @@ import shpallja56 from "../assets/Konkurs - Kryepunëtor i inkasntëve.pdf"
 import inchasant from "../assets/inchasant.JPG"
 import shpallja57 from "../assets/konkursi pf per kapjen e qeneve.pdf"
 import shpallja58 from "../assets/Konkurssi Zyrtar Çertifikues (1).pdf"
+import shpallja59 from "../assets/Konkursi-punetor-fizik.pdf"
 
 const shpalljet = [
+  {
+    id:454,
+    konkurs: "Konkurs",
+    title: "Punëtor fizik për kapjen e qenëve endacak",
+    description: 'Thirrje për kompanitë e interesuara për ofrimin e trajnimeve në krasitje dhe hortikulturë',
+    background: '#003249',
+    buttonColor: "#6ADB84",
+    textColor: "#FFF6E7",
+    adsImage: dog,
+    publicationDate: '29.05.2025',
+    headingTop: "Në bazë të nenit 8 të Ligjit të Punës nr.03/L-212, Rregullores mbi organizimin dhe sistematizimin e vendeve dhe detyrave të punës nr.1292 të datë: 16.06.2023 si dhe Rregullores për procedurat e rekrutimit, Vendimit të Komisionit me numër protokolli 896 të datës 27.03.2024 si dhe Kërkesës me nr Arkivi 888 të dt 27.03.2024 NPL”Gjelbër” rishpall këtë:",
+    titulliPozites: "Kryepunëtor në ngritjen dhe mirëmbajtjen e sipërfaqeve gjelbëruese",
+    kontrata: "puna provuese 6 muaj (me mundësi vazhdimi)",
+    orari: "I plotë",
+    paga: "Niveli i pagës: 10",
+    vendi: "Prishtinë",
+    diploma: "Shkolla e Mesme",
+    patentShofer: "Patentë shofer - Kategoria B",
+    pervojaPunes: "puna provuese 6 muaj (me mundësi vazhdimi)",
+    kushtetKualifikimit: "Të plotësojë kushtet e kualifikimit sipas nenit 17.1 si dhe të plotësojë kriteret e pavarësisë sipas nenit 17.2 pikat (d),(f),(j),(k),(l) të Ligjit nr. 03/L-087 të Ndërmarrjeve Publike dhe Plotësim Ndryshimit të LNP nr. 04/L-111 neni 13;",
+    detyra1: "merr direktiva nga Veterinari dhe Drejtori i Operativës",
+    detyra2: "kryen shërbime për kapjen e qenëve endacak dhe i transporton në qendër",
+    detyra3: "sistemon kafshët në kafaze dhe kujdeset për mirëqenien e tyre",
+    detyra4: "kryen dhe punë të tjera duke u bazuar në veprimtarinë e ndërmarrjes dhe urdhëresës së eprorit,",
+    detyra5: "obligohet të lëviz nga njësia në njësi- zona të tjera, në raste të veçanta dhe sipas kërkesës së punës me urdhër të drejtorit të operativës",
+    detyra6: "për punën e tij i përgjigjet Veterinarit dhe Drejtorit të Operativës",
+    aplikacioniPlotesuar:"Aplikacionin e plotësuar",
+    diplomaNevojshme: "Dëshmitë për kualifikim shkollor",
+    aplikacioniNenBetim: aplikacioni,
+    aplikacioniPerPunesim: aplikacioniPunesimit,
+    aplikimiPDF: shpallja59
+  },
   {
     id:453,
     konkurs: "Konkurs i brendshëm",
