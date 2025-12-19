98ab8f706f88bd5316f53ac2bd21ab2fce752a3b
KreshnikKelmendi <kreshnik.kelmendi1994@gmail.com>
2025-12-19 10:42:34 +0100

    free

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,21 +8,21 @@ import { nextjsUrl } from "../assets/api/nextjsUrl";
 
 const DropdownMenu = ({ closeMenu }) => {
   return (
-    <ul className='absolute bg-[#FFF6E7] z-50 top-full left-0 ml-[-10px] py-5 lg:w-[650px] h-[167px] rounded-[6px]'>
+    <ul className='lg:absolute lg:bg-[#FFF6E7] lg:z-50 lg:top-full lg:left-0 lg:ml-[-10px] lg:py-5 lg:w-[650px] lg:h-[167px] lg:rounded-[6px] lg:shadow-lg space-y-1'>
       <li>
-        <NavLink onClick={closeMenu} to="/komisionikomunal" className='block mx-3 my-2 text-[10px] text-[#003249] font-custom font-normal hover:bg-gray-100'>Komisioni Komunal i Aksionarëve</NavLink>
+        <NavLink onClick={closeMenu} to="/komisionikomunal" className='block px-4 py-2 lg:mx-3 lg:my-2 text-sm lg:text-[10px] text-[#003249] font-custom font-normal hover:bg-[#003249] hover:text-white lg:hover:bg-gray-100 lg:hover:text-[#003249] rounded-lg lg:rounded-none transition-all duration-200'>Komisioni Komunal i Aksionarëve</NavLink>
       </li>
       <li>
-        <NavLink onClick={closeMenu} to="/bordi" className='block mx-3 my-3 text-[10px] text-[#003249] font-custom font-normal hover:bg-gray-100'>Bordi i Drejtorëve</NavLink>
+        <NavLink onClick={closeMenu} to="/bordi" className='block px-4 py-2 lg:mx-3 lg:my-3 text-sm lg:text-[10px] text-[#003249] font-custom font-normal hover:bg-[#003249] hover:text-white lg:hover:bg-gray-100 lg:hover:text-[#003249] rounded-lg lg:rounded-none transition-all duration-200'>Bordi i Drejtorëve</NavLink>
       </li>
       <li>
-        <NavLink onClick={closeMenu} to="/menaxhmenti-i-ndermarrjes" className='block mx-3 my-3 text-[10px] text-[#003249] font-custom font-normal hover:bg-gray-100'>Menaxhmenti i Ndërmarrjes</NavLink>
+        <NavLink onClick={closeMenu} to="/menaxhmenti-i-ndermarrjes" className='block px-4 py-2 lg:mx-3 lg:my-3 text-sm lg:text-[10px] text-[#003249] font-custom font-normal hover:bg-[#003249] hover:text-white lg:hover:bg-gray-100 lg:hover:text-[#003249] rounded-lg lg:rounded-none transition-all duration-200'>Menaxhmenti i Ndërmarrjes</NavLink>
       </li>
       <li>
-        <NavLink onClick={closeMenu} to="/korniza-ligjore" className='block mx-3 my-3 text-[10px] text-[#003249] font-custom font-normal hover:bg-gray-100'>Korniza Ligjore</NavLink>
+        <NavLink onClick={closeMenu} to="/korniza-ligjore" className='block px-4 py-2 lg:mx-3 lg:my-3 text-sm lg:text-[10px] text-[#003249] font-custom font-normal hover:bg-[#003249] hover:text-white lg:hover:bg-gray-100 lg:hover:text-[#003249] rounded-lg lg:rounded-none transition-all duration-200'>Korniza Ligjore</NavLink>
       </li>
       <li>
-        <NavLink onClick={closeMenu} to="/struktura-organizative-e-ndermarrjes" className='block mx-3 my-3 text-[10px] text-[#003249] font-custom font-normal hover:bg-gray-100'>Struktura Organizative e Ndërmarrjes</NavLink>
+        <NavLink onClick={closeMenu} to="/struktura-organizative-e-ndermarrjes" className='block px-4 py-2 lg:mx-3 lg:my-3 text-sm lg:text-[10px] text-[#003249] font-custom font-normal hover:bg-[#003249] hover:text-white lg:hover:bg-gray-100 lg:hover:text-[#003249] rounded-lg lg:rounded-none transition-all duration-200'>Struktura Organizative e Ndërmarrjes</NavLink>
       </li>
     </ul>
   );
@@ -30,12 +30,12 @@ const DropdownMenu = ({ closeMenu }) => {
 
 const ContactDropdownMenu = ({ closeMenu }) => {
   return (
-    <ul className='absolute bg-[#FFF6E7] z-50 top-full left-0 ml-[-10px] py-5 lg:w-[250px] rounded-[6px]'>
+    <ul className='lg:absolute lg:bg-[#FFF6E7] lg:z-50 lg:top-full lg:left-0 lg:ml-[-10px] lg:py-5 lg:w-[250px] lg:rounded-[6px] lg:shadow-lg space-y-1'>
       <li>
-        <NavLink onClick={closeMenu} to="/contact" className='block mx-3 my-2 text-[10px] text-[#003249] font-custom font-normal hover:bg-gray-100'>Kontakti</NavLink>
+        <NavLink onClick={closeMenu} to="/contact" className='block px-4 py-2 lg:mx-3 lg:my-2 text-sm lg:text-[10px] text-[#003249] font-custom font-normal hover:bg-[#003249] hover:text-white lg:hover:bg-gray-100 lg:hover:text-[#003249] rounded-lg lg:rounded-none transition-all duration-200'>Kontakti</NavLink>
       </li>
       <li>
-        <NavLink onClick={closeMenu} to="/zyrtare-pergjegjese-per-sinjalizim" className='block mx-3 my-3 text-[10px] text-[#003249] font-custom font-normal hover:bg-gray-100'>Zyrtare Përgjegjëse për Sinjalizim</NavLink>
+        <NavLink onClick={closeMenu} to="/zyrtare-pergjegjese-per-sinjalizim" className='block px-4 py-2 lg:mx-3 lg:my-3 text-sm lg:text-[10px] text-[#003249] font-custom font-normal hover:bg-[#003249] hover:text-white lg:hover:bg-gray-100 lg:hover:text-[#003249] rounded-lg lg:rounded-none transition-all duration-200'>Zyrtare Përgjegjëse për Sinjalizim</NavLink>
       </li>
     </ul>
   );
@@ -64,10 +64,14 @@ const Header = () => {
     setContactDropdownOpen(!contactDropdownOpen);
   };
 
+  const getShpalljetUrl = () => {
+    return 'https://gjelber-shpalljet.vercel.app/shpalljet';
+  };
+
   return (
-    <div className='w-full sticky'>
-      <div className='lg:flex bg-[#FFF6E7] items-center justify-between py-4 lg:px-10 px-5 z-50'>
-        <div className='font-bold text-2xl cursor-pointer flex items-center font-custom text-gray-800'>
+    <div className='w-full sticky top-0 z-[100]'>
+      <div className='lg:flex bg-[#FFF6E7] items-center justify-between py-4 lg:px-10 px-5 relative z-[100]'>
+        <div className='font-bold text-2xl cursor-pointer flex items-center font-custom text-gray-800 relative z-[101]'>
           <span className='text-3xl text-indigo-600 mr-1 pt-2'>
             <Link to="/" onClick={() => window.scrollTo({ top: 0, left: 0 })}>
               <img className='w-[127px] h-auto' src={logo} alt='' />
@@ -75,26 +79,160 @@ const Header = () => {
           </span>
         </div>
 
-        <div onClick={toggleMenu} className='text-3xl absolute right-5 top-5 cursor-pointer lg:hidden'>
+        <button 
+          onClick={toggleMenu} 
+          className='text-3xl absolute right-5 top-5 cursor-pointer lg:hidden z-[101] p-2'
+          aria-label="Toggle menu"
+        >
           {open ? (
-            <img className='w-7 h-7 object-cover' src={closeIcon} alt='' />
+            <img className='w-7 h-7 object-cover' src={closeIcon} alt='Close menu' />
           ) : (
-            <img className='w-7 h-7 object-cover' src={menu} alt='' />
+            <img className='w-7 h-7 object-cover' src={menu} alt='Open menu' />
           )}
+        </button>
+
+        {/* Mobile Menu Overlay */}
+        {open && (
+          <div 
+            className='lg:hidden fixed inset-0 bg-black bg-opacity-60 z-[99] transition-opacity duration-300'
+            onClick={closeMenu}
+          />
+        )}
+
+        {/* Mobile Menu Sidebar */}
+        <div className={`lg:hidden fixed top-0 right-0 h-screen w-[85%] max-w-sm bg-[#FFF6E7] z-[100] shadow-2xl transition-transform duration-300 ease-in-out ${open ? 'translate-x-0' : 'translate-x-full'}`}>
+          {/* Close Button */}
+          <div className='flex justify-end p-5 border-b border-gray-200'>
+            <button onClick={closeMenu} className='p-2 hover:bg-gray-100 rounded-full transition-colors'>
+              <img className='w-6 h-6 object-cover' src={closeIcon} alt='Close menu' />
+            </button>
+          </div>
+
+          {/* Menu Items */}
+          <div className='h-[calc(100vh-80px)] overflow-y-auto px-6 py-8'>
+            <ul className='flex flex-col space-y-1'>
+              <li>
+                <NavLink 
+                  onClick={closeMenu} 
+                  exact 
+                  to="/" 
+                  className='block px-4 py-3 text-gray-800 hover:bg-[#003249] hover:text-white rounded-lg transition-all duration-200 font-custom text-base'
+                  activeClassName='bg-[#003249] text-white'
+                >
+                  Ballina
+                </NavLink>
+              </li>
+              
+              <li>
+                <div>
+                  <div
+                    onClick={toggleDropdown}
+                    className='flex items-center justify-between px-4 py-3 text-gray-800 hover:bg-[#003249] hover:text-white rounded-lg transition-all duration-200 cursor-pointer font-custom text-base'
+                  >
+                    <NavLink onClick={closeMenu} to="/rrethNesh" className='flex-1'>Rreth Nesh</NavLink>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      className={`w-5 h-5 transition-transform duration-200 ${dropdownOpen ? 'rotate-180' : ''}`}
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M19 9l-7 7-7-7"
+                      />
+                    </svg>
+                  </div>
+                  {dropdownOpen && (
+                    <div className='mt-1 ml-4 space-y-1 border-l-2 border-gray-300 pl-4'>
+                      <DropdownMenu closeMenu={closeMenu} />
+                    </div>
+                  )}
+                </div>
+              </li>
+
+              <li>
+                <NavLink 
+                  onClick={closeMenu} 
+                  to="/aktivitetet" 
+                  className='block px-4 py-3 text-gray-800 hover:bg-[#003249] hover:text-white rounded-lg transition-all duration-200 font-custom text-base'
+                  activeClassName='bg-[#003249] text-white'
+                >
+                  Aktivitetet
+                </NavLink>
+              </li>
+
+              <li>
+                <NavLink 
+                  onClick={closeMenu} 
+                  to="/veprimtarite" 
+                  className='block px-4 py-3 text-gray-800 hover:bg-[#003249] hover:text-white rounded-lg transition-all duration-200 font-custom text-base'
+                  activeClassName='bg-[#003249] text-white'
+                >
+                  Veprimtaritë
+                </NavLink>
+              </li>
+
+              <li>
+                <div>
+                  <div
+                    onClick={toggleContactDropdown}
+                    className='flex items-center justify-between px-4 py-3 text-gray-800 hover:bg-[#003249] hover:text-white rounded-lg transition-all duration-200 cursor-pointer font-custom text-base'
+                  >
+                    <NavLink onClick={closeMenu} to="/contact" className='flex-1'>Kontakti</NavLink>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      className={`w-5 h-5 transition-transform duration-200 ${contactDropdownOpen ? 'rotate-180' : ''}`}
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M19 9l-7 7-7-7"
+                      />
+                    </svg>
+                  </div>
+                  {contactDropdownOpen && (
+                    <div className='mt-1 ml-4 space-y-1 border-l-2 border-gray-300 pl-4'>
+                      <ContactDropdownMenu closeMenu={closeMenu} />
+                    </div>
+                  )}
+                </div>
+              </li>
+
+              <li className='pt-4 mt-4 border-t border-gray-200'>
+                <a 
+                  onClick={closeMenu} 
+                  href={getShpalljetUrl()}
+                  className='block w-full'
+                >
+                  <Button className='w-full text-center text-white hover:text-white duration-500 active:text-red-500'>
+                    Shpalljet
+                  </Button>
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
 
-        <ul className={`lg:flex lg:items-center lg:pb-0 pb-12 absolute font-normal text-[15px] lg:text-[13px] lg:static bg-[#FFF6E7] lg:z-auto z-50 lg:space-x-14 left-0 w-full lg:w-auto lg:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-18' : 'top-[-490px]'}`}>
-          <li className='lg:ml-8 lg:my-0 my-7 font-custom'>
+        {/* Desktop Menu */}
+        <ul className='hidden lg:flex lg:items-center lg:pb-0 lg:space-x-14 font-normal text-[13px]'>
+          <li className='lg:ml-8 font-custom'>
             <NavLink onClick={closeMenu} exact to="/" className='text-gray-800 hover:text-gray-400 duration-500 active:text-red-500'>Ballina</NavLink>
           </li>
-          <li className='lg:ml-8 lg:my-0 my-7 font-custom relative'>
+          <li className='lg:ml-8 font-custom relative'>
             <div
               onMouseEnter={toggleDropdown}
               onMouseLeave={toggleDropdown}
               className='text-gray-800 hover:text-gray-400 duration-500 active:text-red-500 cursor-pointer flex items-center'
             >
               <NavLink onClick={closeMenu} to="/rrethNesh">Rreth Nesh</NavLink>
-              <span className=' ml-8 lg:ml-3'>
+              <span className='ml-3'>
                 {dropdownOpen ? (
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -130,20 +268,20 @@ const Header = () => {
               {dropdownOpen && <DropdownMenu closeMenu={closeMenu} />}
             </div>
           </li>
-          <li className='lg:ml-8 lg:my-0 my-7 font-custom'>
+          <li className='lg:ml-8 font-custom'>
             <NavLink onClick={closeMenu} to="/aktivitetet" className='text-gray-800 hover:text-gray-400 duration-500 active:text-red-500'>Aktivitetet</NavLink>
           </li>
-          <li className='lg:ml-8 lg:my-0 my-7 font-custom'>
+          <li className='lg:ml-8 font-custom'>
             <NavLink onClick={closeMenu} to="/veprimtarite" className='text-gray-800 hover:text-gray-400 duration-500 active:text-red-500'>Veprimtaritë</NavLink>
           </li>
-          <li className='lg:ml-8 lg:my-0 my-7 font-custom relative'>
+          <li className='lg:ml-8 font-custom relative'>
             <div
               onMouseEnter={toggleContactDropdown}
               onMouseLeave={toggleContactDropdown}
               className='text-gray-800 hover:text-gray-400 duration-500 active:text-red-500 cursor-pointer flex items-center'
             >
               <NavLink onClick={closeMenu} to="/contact">Kontakti</NavLink>
-              <span className=' ml-8 lg:ml-3'>
+              <span className='ml-3'>
                 {contactDropdownOpen ? (
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -179,7 +317,7 @@ const Header = () => {
               {contactDropdownOpen && <ContactDropdownMenu closeMenu={closeMenu} />}
             </div>
           </li>
-          <a onClick={closeMenu} href={nextjsUrl ? `${nextjsUrl}/shpalljet` : '/shpalljet'} className='z-50'>
+          <a onClick={closeMenu} href={getShpalljetUrl()}>
             <Button className='text-white hover:text-white duration-500 active:text-red-500'>
               Shpalljet
             </Button>
--- src/components/home/SliderHome.jsx
+++ src/components/home/SliderHome.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 import sliderImage1 from "../assets/sliderHome.png";
 import sliderImage2 from "../assets/slide2.jpg";
 
@@ -7,19 +8,24 @@ const SliderHome = () => {
 
   const slides = [
     {
-      title: "Prishtina më Gjelbër jeta më e bukur.",
-      content: "Bashkohu në Mbrojtjen e Mjedisit!",
+      title: "Prishtina më Gjelbër",
+      subtitle: "jeta më e bukur",
+      content: "Bashkohu në Mbrojtjen e Mjedisit dhe kontribuo për një qytet më të gjelbër dhe më të pastër.",
       image: sliderImage1,
       backgroundColor: '#6ADB84',
-      textColor: '#003249', 
+      textColor: '#003249',
+      buttonColor: '#003249',
+      buttonTextColor: '#FFF6E7',
     },
-
     {
-      title: "Bashkohu në Mbrojtjen e Mjedisit!",
-      content: "Prishtina më Gjelbër jeta më e bukur.",
+      title: "Bashkohu në Mbrojtjen",
+      subtitle: "e Mjedisit",
+      content: "Prishtina më Gjelbër jeta më e bukur. Bashkëpunojmë për një të ardhme më të qëndrueshme.",
       image: sliderImage2,
       backgroundColor: '#003249',
-      textColor: '#6ADB84', 
+      textColor: '#6ADB84',
+      buttonColor: '#6ADB84',
+      buttonTextColor: '#003249',
     },
   ];
 
@@ -28,22 +34,142 @@ const SliderHome = () => {
   };
 
   useEffect(() => {
-    const intervalId = setInterval(changeSlide, 3000); 
-
-    return () => clearInterval(intervalId); 
+    const intervalId = setInterval(changeSlide, 5000);
+    return () => clearInterval(intervalId);
   }, []);
 
+  const goToSlide = (index) => {
+    setCurrentSlide(index);
+  };
+
+  const getShpalljetUrl = () => {
+    return 'https://gjelber-shpalljet.vercel.app/shpalljet';
+  };
+
+  const currentSlideData = slides[currentSlide];
+
   return (
-    <div className='w-full lg:h-[734px] flex z-30' style={{ backgroundColor: slides[currentSlide].backgroundColor }}>
-      <div className='lg:ml-10 grid lg:block px-5 lg:px-0 lg:py-[150px] w-1/2 justify-center items-center h-[30vh]'>
-        <p className={`text-2xl lg:text-[96px] font-custom lg:leading-[101px] w-full lg:w-fit font-bold`} style={{ color: slides[currentSlide].textColor }}>
-          {slides[currentSlide].title}
-        </p>
-        <p className='text-base lg:mt-10 lg:text-[34px] text-[#FFF6E7] font-custom font-normal'>{slides[currentSlide].content}</p>
+    <div className='w-full relative h-[40vh] lg:h-[734px] overflow-hidden z-0'>
+      <AnimatePresence mode="wait">
+        <motion.div
+          key={currentSlide}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.6 }}
+          className='w-full h-full flex flex-row absolute inset-0'
+          style={{ backgroundColor: currentSlideData.backgroundColor }}
+        >
+          {/* Content Section */}
+          <div className='w-1/2 flex flex-col justify-center items-start px-3 sm:px-5 lg:px-10 lg:pl-16 py-8 lg:py-0 '>
+            <motion.div
+              key={`content-${currentSlide}`}
+              initial={{ x: -50, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              exit={{ x: 50, opacity: 0 }}
+              transition={{ delay: 0.1, duration: 0.6 }}
+              className='max-w-2xl'
+            >
+              <h1 
+                className='text-xl sm:text-2xl md:text-3xl lg:text-6xl xl:text-7xl font-custom font-bold mb-2 sm:mb-3 lg:mb-6 leading-tight'
+                style={{ color: currentSlideData.textColor }}
+              >
+                {currentSlideData.title}
+                <br />
+                <span className='text-[#FFF6E7]'>{currentSlideData.subtitle}</span>
+              </h1>
+              
+              <p 
+                className='text-xs sm:text-sm md:text-base lg:text-xl text-[#FFF6E7] font-custom font-normal mb-4 sm:mb-6 lg:mb-10 leading-relaxed max-w-xl'
+              >
+                {currentSlideData.content}
+              </p>
+
+              {/* Shpalljet Button */}
+              <motion.a
+                href={getShpalljetUrl()}
+                onClick={() => window.scrollTo({ top: 0, left: 0 })}
+                initial={{ scale: 0.9, opacity: 0 }}
+                animate={{ scale: 1, opacity: 1 }}
+                exit={{ scale: 0.9, opacity: 0 }}
+                transition={{ delay: 0.3, duration: 0.5 }}
+                className='inline-block'
+              >
+                <button
+                  className='px-4 py-2 sm:px-6 sm:py-3 lg:px-10 lg:py-5 rounded-xl font-custom font-bold text-xs sm:text-sm lg:text-lg shadow-2xl hover:shadow-3xl transform hover:scale-105 transition-all duration-300 flex items-center gap-2 lg:gap-3'
+                  style={{ 
+                    backgroundColor: currentSlideData.buttonColor,
+                    color: currentSlideData.buttonTextColor
+                  }}
+                >
+                  <span>Shpalljet</span>
+                  <svg className="w-3 h-3 sm:w-4 sm:h-4 lg:w-5 lg:h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 8l4 4m0 0l-4 4m4-4H3" />
+                  </svg>
+                </button>
+              </motion.a>
+            </motion.div>
+          </div>
+
+          {/* Image Section */}
+          <div className='w-1/2 relative h-full overflow-hidden'>
+            <motion.img
+              key={`image-${currentSlide}`}
+              src={currentSlideData.image}
+              alt={currentSlideData.title}
+              initial={{ scale: 1.1, opacity: 0 }}
+              animate={{ scale: 1, opacity: 1 }}
+              exit={{ scale: 1.1, opacity: 0 }}
+              transition={{ duration: 0.8 }}
+              className='w-full h-full object-cover'
+            />
+            {/* Gradient Overlay */}
+            <div 
+              className='absolute inset-0 opacity-20'
+              style={{ 
+                background: `linear-gradient(to left, ${currentSlideData.backgroundColor}, transparent)` 
+              }}
+            />
+          </div>
+        </motion.div>
+      </AnimatePresence>
+
+      {/* Slide Indicators */}
+      <div className='absolute bottom-4 lg:bottom-6 left-1/2 transform -translate-x-1/2 flex gap-2 lg:gap-3'>
+        {slides.map((_, index) => (
+          <button
+            key={index}
+            onClick={() => goToSlide(index)}
+            className={`h-2 lg:h-3 rounded-full transition-all duration-300 ${
+              index === currentSlide 
+                ? 'bg-white w-8 lg:w-8' 
+                : 'bg-white bg-opacity-50 hover:bg-opacity-75 w-2 lg:w-3'
+            }`}
+            aria-label={`Go to slide ${index + 1}`}
+          />
+        ))}
       </div>
-      <div className='w-1/2 '>
-        <img src={slides[currentSlide].image} alt='' className='lg:w-full w-[50vh] h-[30vh] lg:h-[662px] object-cover' />
-      </div>
+
+      {/* Navigation Arrows - Hidden on mobile, shown on desktop */}
+      <button
+        onClick={() => setCurrentSlide((prev) => (prev - 1 + slides.length) % slides.length)}
+        className='absolute left-2 lg:left-4 top-1/2 transform -translate-y-1/2 z-10 bg-white bg-opacity-20 hover:bg-opacity-30 backdrop-blur-sm rounded-full p-2 lg:p-3 transition-all duration-300 hidden lg:block'
+        aria-label="Previous slide"
+      >
+        <svg className="w-5 h-5 lg:w-6 lg:h-6 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-7-7 7-7" />
+        </svg>
+      </button>
+      
+      <button
+        onClick={() => setCurrentSlide((prev) => (prev + 1) % slides.length)}
+        className='absolute right-2 lg:right-4 top-1/2 transform -translate-y-1/2 z-10 bg-white bg-opacity-20 hover:bg-opacity-30 backdrop-blur-sm rounded-full p-2 lg:p-3 transition-all duration-300 hidden lg:block'
+        aria-label="Next slide"
+      >
+        <svg className="w-5 h-5 lg:w-6 lg:h-6 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7" />
+        </svg>
+      </button>
     </div>
   );
 }
